e86ae999809ee4de19eac65d3b15966dc0903089
Caleb Cushing <xenoterracide@gmail.com>
2013-10-28 03:56:23 -0500

    add stub
    Signed-off-by: Caleb Cushing <xenoterracide@gmail.com>

--- lib/Role/Equals.pm
+++ lib/Role/Equals.pm
@@ -2,9 +2,20 @@ package Role::Equals;
 use 5.006;
 use strict;
 use warnings;
+use Moo::Role;
 
 # VERSION
 
+sub equals {
+	my ( $self, $obj ) = @_;
+
+	return;
+}
+
+with qw(
+	Interface::Equatable
+);
+
 1;
 
-# ABSTRACT: Role::Equals
+# ABSTRACT: dumb equatable
